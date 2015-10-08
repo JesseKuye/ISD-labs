4d05f29f8d71861e72b57424cbe3816b6f07dc52
Jesse <jessekuye@gmail.com>
2015-10-08 19:03:38 +0100

    Create Week 2 question 12.py

---
+++ Week 2 question 12.py
@@ -0,0 +1,8 @@
+#asks user for their name
+name1= input("Hello, What is your name? ")
+
+#asks user for their age
+yoa= int(input ("How old are you? "))
+
+#uses name and age to print the statement
+print ("Hello,", name1,"next year you will be", yoa+1,)
